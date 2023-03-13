b528d4f1ef8daffcb037a2a730eca695ce301cea
AMIHbKA <bishop0013forum@gmail.com>
2023-03-13 07:11:52 +0200

    intermediate result

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+export function fetchCountries(name) {
+  const API_URL = 'https://restcountries.com/v3.1/name/';
+  return fetch(
+    `${API_URL}${name}?fields=name,capital,population,languages,flags`
+  ).then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Oops, there is no country with that name');
+      } else {
+        throw new Error(response.status);
+      }
+    }
+
+    return response.json();
+  });
+}
